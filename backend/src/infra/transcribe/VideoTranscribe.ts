b5f07ca5541836ff0f56967446d1332d503db26b
Paulo Abdiel <abdielpaulo2@gmail.com>
2023-10-29 22:10:26 -0300

    formatando codigo

--- backend/src/infra/transcribe/VideoTranscribe.ts
+++ backend/src/infra/transcribe/VideoTranscribe.ts
@@ -12,7 +12,7 @@ export class VideoTranscriber {
         this.transcriptFormated = "";
     }
 
-    public async transcribe(videoFile: any){
+    public async transcribe(videoFile: any) {
         const file = {
             buffer: fs.readFileSync(videoFile),
             mimetype: 'audio/mp3',
@@ -39,10 +39,10 @@ export class VideoTranscriber {
         }
     }
 
-    public async transcriptFormatedData(transcript: string){
+    public async transcriptFormatedData(transcript: string) {
         const lines = transcript.split('\n');
 
-    // Inicialize o dicionário
+        // Inicialize o dicionário
         const subtitles: any = {};
 
         // Variáveis para rastrear tempo e texto
@@ -50,22 +50,22 @@ export class VideoTranscriber {
 
         // Analise as linhas
         for (const line of lines) {
-        if (line.includes('-->')) {
-            // Esta linha contém informações de tempo
-            const [startTime, endTime] = line.split(' --> ');
-            currentTime = `${startTime} --> ${endTime}`;
-        } else if (line.trim() !== '' && currentTime) {
-            // Esta linha contém texto
-            if (!subtitles[currentTime]) {
-            subtitles[currentTime] = '';
+            if (line.includes('-->')) {
+                // Esta linha contém informações de tempo
+                const [startTime, endTime] = line.split(' --> ');
+                currentTime = `${startTime} --> ${endTime}`;
+            } else if (line.trim() !== '' && currentTime) {
+                // Esta linha contém texto
+                if (!subtitles[currentTime]) {
+                    subtitles[currentTime] = '';
+                }
+                subtitles[currentTime] += `${line} `;
             }
-            subtitles[currentTime] += `${line} `;
-        }
         }
 
         // Remova espaços em branco no início e no final de cada texto
         for (const key in subtitles) {
-        subtitles[key] = subtitles[key].trim();
+            subtitles[key] = subtitles[key].trim();
         }
 
         return subtitles;
